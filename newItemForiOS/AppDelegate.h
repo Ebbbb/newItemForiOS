34e348addc61ab1621fa529ba70db4eaf7016e72
蔡源 <894054389@qq.com>
2018-06-01 13:25:33 +0800

    first commit

---
+++ newItemForiOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  newItemForiOS
+//
+//  Created by 蔡源 on 2018/6/1.
+//  Copyright © 2018年 蔡源. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ newItemForiOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  newItemForiOS
+//
+//  Created by 蔡源 on 2018/6/1.
+//  Copyright © 2018年 蔡源. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
